77c500a9cf7d7fc88785e51943875daf908b67db
omer-fsdev <tbp.omerkoyuncu@gmail.com>
2023-05-18 13:58:14 +0200

    edit

--- src/data.js
+++ src/data.js
@@ -4,28 +4,28 @@ const data = [
     name: "SpongeBob",
     job: "Developer",
     comment: "I really like the app...",
-    img: "http://de.spongepedia.org/images/55a_SpongeBob.jpg",
+    img: "https://student-editorials.blogs.auckland.ac.nz/files/2022/08/spongebob.jpg",
   },
   {
     id: 2,
     name: "Patrick Star",
     job: "Tester",
     comment: "The application is very difficult.",
-    img: "http://de.spongepedia.org/images/89b_Patrick.jpg",
+    img: "https://i.pinimg.com/originals/dc/a5/8d/dca58d1a552b4ae61b447f51271020a8.jpg",
   },
   {
     id: 3,
     name: "Karen",
     job: "Graphic Designer",
     comment: "Congratulations, very successful!",
-    img: "http://de.spongepedia.org/images/MobileKaren.jpg",
+    img: "https://i.pinimg.com/originals/e6/a3/1a/e6a31a204e04ff6f54c094c249ab17e7.png",
   },
   {
     id: 4,
     name: "Thaddäus Tentakel",
     job: "Trainee",
     comment: "It needs to be developed further.",
-    img: "http://de.spongepedia.org/images/thumb/56b_Thadd%C3%A4us.jpg/350px-56b_Thadd%C3%A4us.jpg",
+    img: "https://images-na.ssl-images-amazon.com/images/I/51MnIoXSZaL._AC_UL600_SR600,600_.jpg",
   },
 ];
 
